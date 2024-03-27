575235230dfe15d16e60181e4abc28f98b269f58
Michael Wekesa <wekesa360@outlook.com>
2024-03-27 18:19:17 +0300

    Update toast autoClose duration

--- client/src/components/common/Toast/Toast.jsx
+++ client/src/components/common/Toast/Toast.jsx
@@ -4,44 +4,48 @@ const Toast = {
   success: (message) => {
     toast.success(message, {
       position: 'top-right',
-      autoClose: 5000,
+      autoClose: 4000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
+      theme: "colored",
       progress: undefined,
     });
   },
   error: (message) => {
     toast.error(message, {
       position: 'top-right',
-      autoClose: 5000,
+      autoClose: 4000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
+      theme: "colored",
       progress: undefined,
     });
   },
   info: (message) => {
     toast.info(message, {
       position: 'top-right',
-      autoClose: 5000,
+      autoClose: 4000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
+      theme: "colored",
       progress: undefined,
     });
   },
   warning: (message) => {
     toast.warning(message, {
       position: 'top-right',
-      autoClose: 5000,
+      autoClose: 4000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
+      theme: "colored",
       progress: undefined,
     });
   },
